9564374d633661d718ff7a668ccbbfba452c930a
Sahan Sarki <sahancansarki@gmail.com>
2022-03-15 19:13:09 +0300

    MainActivity's location was changed.

--- app/src/main/java/com/example/calendarcase/presentation/screen/MainActivity.kt
+++ app/src/main/java/com/example/calendarcase/presentation/screen/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.calendarcase
+package com.example.calendarcase.presentation.screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calendarcase.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
