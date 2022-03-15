c03ab48486679349d4be3d27b962849f88325265
Sahan Sarki <sahancansarki@gmail.com>
2022-03-15 21:52:12 +0300

    binding code has been added.

--- app/src/main/java/com/example/calendarcase/presentation/screen/MainActivity.kt
+++ app/src/main/java/com/example/calendarcase/presentation/screen/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.calendarcase.presentation.screen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calendarcase.R
+import com.example.calendarcase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityBinding.root)
+
+        activityBinding.floatingActionButton.setOnClickListener {
+
+        }
     }
 }
