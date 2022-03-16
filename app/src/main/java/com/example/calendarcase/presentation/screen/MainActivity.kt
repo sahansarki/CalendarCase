f951415c462c164d86226dded1b64f3b9be4f9cc
Sahan Sarki <sahancansarki@gmail.com>
2022-03-16 11:37:20 +0300

    BottomSheet was connected to the floating button.

--- app/src/main/java/com/example/calendarcase/presentation/screen/MainActivity.kt
+++ app/src/main/java/com/example/calendarcase/presentation/screen/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calendarcase.R
 import com.example.calendarcase.databinding.ActivityMainBinding
+import com.example.calendarcase.presentation.screen.popup.BottomSheetFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var activityBinding: ActivityMainBinding
@@ -14,7 +15,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(activityBinding.root)
 
         activityBinding.floatingActionButton.setOnClickListener {
-
+            val bottomSheet = BottomSheetFragment.newInstance()
+            bottomSheet.show(
+                supportFragmentManager,
+                BottomSheetFragment.TAG
+            )
         }
     }
 }
