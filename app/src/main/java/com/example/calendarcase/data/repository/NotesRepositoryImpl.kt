05bea9b8393610ebd4fb74160c432297df4c2f71
Sahan Sarki <sahancansarki@gmail.com>
2022-03-17 17:57:19 +0300

    Repository was created.

--- app/src/main/java/com/example/calendarcase/data/repository/NotesRepositoryImpl.kt
+++ app/src/main/java/com/example/calendarcase/data/repository/NotesRepositoryImpl.kt
@@ -2,21 +2,57 @@ package com.example.calendarcase.data.repository
 
 import com.example.calendarcase.data.local.NotesDao
 import com.example.calendarcase.data.local.model.NoteEntity
+import com.example.calendarcase.domain.model.Note
 import com.example.calendarcase.domain.repository.NotesRepository
+import com.example.calendarcase.domain.repository.mapper.mapNoteEntity
+import com.example.calendarcase.domain.repository.mapper.mapNoteToEntity
+import com.example.calendarcase.util.CalendarError
+import com.example.calendarcase.util.DataHolder
+import java.lang.Exception
 import javax.inject.Inject
 
 class NotesRepositoryImpl @Inject constructor(
     private val notesDao: NotesDao
 ): NotesRepository {
-    override suspend fun insertNote(note: NoteEntity) {
-        notesDao.insertNote(note)
+    override suspend fun insertNote(note: Note): DataHolder<Note> {
+
+        val result: DataHolder<Note> = try{
+            val data = mapNoteToEntity(note)
+            notesDao.insertNote(data)
+
+            DataHolder.success(note)
+        } catch (e: Exception){
+            DataHolder.error(CalendarError(e.localizedMessage,e) ,note)
+        }
+
+        return result
+
     }
 
-    override suspend fun deleteNote(note: NoteEntity) {
-        notesDao.deleteNote(note)
+    override suspend fun deleteNote(note: Note): DataHolder<Note> {
+
+        val result: DataHolder<Note> = try{
+            val data = mapNoteToEntity(note)
+            notesDao.deleteNote(data)
+
+            DataHolder.success(note)
+        } catch (e: Exception){
+            DataHolder.error(CalendarError(e.localizedMessage, e),note)
+        }
+
+        return  result
     }
 
-    override suspend fun getNoteByDate(date: String): NoteEntity {
-        return notesDao.getNoteByDate(date)
+    override suspend fun getNoteByDate(date: String): DataHolder<Note> {
+
+        val result: DataHolder<Note> = try{
+            val noteEntity = notesDao.getNoteByDate(date)
+            val dataNote = mapNoteEntity(noteEntity)
+            DataHolder.success(dataNote)
+        } catch (e: Exception){
+            DataHolder.error(CalendarError(e.localizedMessage, e),null)
+        }
+
+        return result
     }
 }
--- app/src/main/java/com/example/calendarcase/domain/repository/NotesRepository.kt
+++ app/src/main/java/com/example/calendarcase/domain/repository/NotesRepository.kt
@@ -2,9 +2,11 @@ package com.example.calendarcase.domain.repository
 
 
 import com.example.calendarcase.data.local.model.NoteEntity
+import com.example.calendarcase.domain.model.Note
+import com.example.calendarcase.util.DataHolder
 
 interface NotesRepository {
-    suspend fun insertNote(note: NoteEntity)
-    suspend fun deleteNote(note: NoteEntity)
-    suspend fun getNoteByDate(date: String): NoteEntity
+    suspend fun insertNote(note: Note): DataHolder<Note>
+    suspend fun deleteNote(note: Note): DataHolder<Note>
+    suspend fun getNoteByDate(date: String): DataHolder<Note>
 }
