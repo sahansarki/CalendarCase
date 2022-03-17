72391d4fc965b112e36cfaefea54757cb6afe679
Sahan Sarki <sahancansarki@gmail.com>
2022-03-17 20:29:50 +0300

    delete action was added.

--- app/src/main/java/com/example/calendarcase/presentation/screen/main/MainActivityViewModel.kt
+++ app/src/main/java/com/example/calendarcase/presentation/screen/main/MainActivityViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.calendarcase.domain.model.Note
+import com.example.calendarcase.domain.usecase.DeleteNoteUseCase
 import com.example.calendarcase.domain.usecase.GetNoteByDateUseCase
+import com.example.calendarcase.enum.RepositoryStatus
 import com.example.calendarcase.util.DataHolder
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -14,7 +16,8 @@ import javax.inject.Inject
 @DelicateCoroutinesApi
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
-    private val getNoteByDateUseCase: GetNoteByDateUseCase
+    private val getNoteByDateUseCase: GetNoteByDateUseCase,
+    private val deleteNoteUseCase: DeleteNoteUseCase,
 ): ViewModel() {
 
     private val mutableNote =  MutableLiveData<DataHolder<Note>>()
@@ -24,7 +27,14 @@ class MainActivityViewModel @Inject constructor(
     fun getNoteByDate(date: String){
         getNoteByDateUseCase(date, viewModelScope){
             mutableNote.value = it
+        }
+    }
 
+    fun deleteNote(note: Note, success: (text: String) -> Unit){
+        deleteNoteUseCase(note, viewModelScope){
+            if(it.status == RepositoryStatus.OK){
+                success("${it.data!!.title} has been deleted.")
+            }
         }
     }
 
