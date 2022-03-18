74c80caa849034d657ad66c914e57b3495430d53
Sahan Sarki <sahancansarki@gmail.com>
2022-03-18 18:44:30 +0300

    Adapter was added to activity.

--- app/src/main/java/com/example/calendarcase/presentation/screen/main/MainActivity.kt
+++ app/src/main/java/com/example/calendarcase/presentation/screen/main/MainActivity.kt
@@ -7,12 +7,13 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.calendarcase.databinding.ActivityMainBinding
 import com.example.calendarcase.enum.Month
 import com.example.calendarcase.enum.RepositoryStatus
-import com.example.calendarcase.extension.clearMainLayout
 import com.example.calendarcase.extension.dateConvert
-import com.example.calendarcase.extension.viewMainLayout
+import com.example.calendarcase.presentation.adapter.NotesRecyclerAdapter
 import com.example.calendarcase.presentation.screen.popup.BottomSheetFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -25,6 +26,8 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var activityBinding: ActivityMainBinding
     private val mViewModel: MainActivityViewModel by viewModels()
+    private lateinit var noteAdapter: NotesRecyclerAdapter
+    private lateinit var dateString: String
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,13 +40,41 @@ class MainActivity : AppCompatActivity() {
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-        val dateString = dateConvert(year, month, day)
+        dateString = dateConvert(year, month, day)
+
+        noteAdapter = NotesRecyclerAdapter({ updatedNote ->
+
+            val bottomSheet = BottomSheetFragment.newInstance(updatedNote){
+                mViewModel.getNoteByDate(it)
+            }
+            bottomSheet.show(
+                supportFragmentManager,
+                BottomSheetFragment.TAG
+            )
+        }, { deletedNote ->
+            mViewModel.deleteNote(deletedNote){
+                mViewModel.getNoteByDate(dateString)
+            }
+        })
+
+        val mDividerItemDecoration =
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+
+        with(activityBinding.noteListRc){
+            layoutManager =
+                LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+            adapter = noteAdapter
+            addItemDecoration(mDividerItemDecoration)
+            setHasFixedSize(true)
+        }
+
+
         mViewModel.getNoteByDate(dateString)
 
 
         activityBinding.floatingActionButton.setOnClickListener {
-            val bottomSheet = BottomSheetFragment.newInstance(){
-
+            val bottomSheet = BottomSheetFragment.newInstance{
+                mViewModel.getNoteByDate(dateString)
             }
             bottomSheet.show(
                 supportFragmentManager,
@@ -62,49 +93,19 @@ class MainActivity : AppCompatActivity() {
                 "${date.format(dayOfWeekFormatter)}, ${Month.values()[mMonth].name} $mDay, $mYear"
             mViewModel.getNoteByDate(dateString)
         }
-
-        activityBinding.buttonDelete.setOnClickListener {
-            if(activityBinding.dateText.visibility == View.VISIBLE) {
-                mViewModel.deleteNote(activityBinding.note!!){
-                    Toast.makeText(this, it, Toast.LENGTH_LONG).show()
-                    clearMainLayout(activityBinding)
-                }
-            }
-        }
-
-        activityBinding.buttonUpdate.setOnClickListener {
-            val bottomSheet = BottomSheetFragment.newInstance(activityBinding.note!!){
-                mViewModel.getNoteByDate(it)
-            }
-            bottomSheet.show(
-                supportFragmentManager,
-                BottomSheetFragment.TAG
-            )
-        }
         observeNote()
     }
-
-
-
-
-
 
 
     private fun observeNote() {
         mViewModel.note.observe(this) {
             when (it.status) {
                 RepositoryStatus.OK -> {
-                    val note = it.data!!
-                    viewMainLayout(activityBinding)
-                    activityBinding.note = note
-                    activityBinding.allDaySwitchButton.isChecked = note.allDay
-                    activityBinding.replaySwitchButton.isChecked = note.doesRepeat
-                    activityBinding.allDaySwitchButton.isClickable = false
-                    activityBinding.replaySwitchButton.isClickable = false
+                    val noteList = it.data!!
+                    noteAdapter.differ.submitList(noteList)
                 }
 
                 RepositoryStatus.ERROR -> {
-                    clearMainLayout(activityBinding)
                     Toast.makeText(this, it.error!!.message, Toast.LENGTH_LONG).show()
                 }
             }
